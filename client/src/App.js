413d435b95be610d12d192db8b411ac622f25570
Emmanuel <fegomson@gmail.com>
2020-10-16 08:58:24 -0500

    Load user and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Alert from "./components/layout/Alert"
+import setAuthToken from "./utils/setAuthToken"
+import { loadUser } from "./actions/auth"
 
 // Redux Setup
 import { Provider } from "react-redux"
@@ -12,7 +14,17 @@ import store from "./store"
 
 import './App.css';
 
+// Check if there's a token and put it in the global header
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
 const App = () => {
+  // useEffect for loadUser action
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return(
       <Provider store={store}>
         <Router>
